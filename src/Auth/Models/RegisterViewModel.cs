ca8db2a63a7f9af526cfb50e3ed36677c2f6a68c
pin jui <fee8ack21@gmail.com>
2026-06-02 19:13:47 +0800

    refactor(auth): 簡化註冊表單，移除暱稱欄位並新增確認密碼

--- src/Auth/Models/RegisterViewModel.cs
+++ src/Auth/Models/RegisterViewModel.cs
@@ -4,9 +4,6 @@ namespace Auth.Models;
 
 public class RegisterViewModel
 {
-    [Required(ErrorMessage = "請輸入你的暱稱")]
-    public string DisplayName { get; set; } = "";
-
     [Required(ErrorMessage = "請輸入電子信箱")]
     [EmailAddress(ErrorMessage = "請輸入正確的電子信箱")]
     public string Email { get; set; } = "";
@@ -14,6 +11,10 @@ public class RegisterViewModel
     [Required(ErrorMessage = "請設定密碼")]
     [MinLength(8, ErrorMessage = "密碼至少需要 8 個字元")]
     public string Password { get; set; } = "";
+
+    [Required(ErrorMessage = "請再次輸入密碼")]
+    [Compare("Password", ErrorMessage = "兩次密碼不一致")]
+    public string ConfirmPassword { get; set; } = "";
 
     [MustBeTrue(ErrorMessage = "請先同意服務條款")]
     public bool Agree { get; set; }
